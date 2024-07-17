3d0fa50fa55998630a66b1d56ec85b45a3914a44
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2024-07-17 21:30:20 +0500

    Add dashboard

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import PriceList from '@/components/component/price-list';
+import Component from '../components/component2/Component';
+
+export default function Home() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
